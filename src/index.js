8e11ad4b3bd7f0d8cc45211e60fbe454e77c0b37
di330nance <di330nance@gmail.com>
2025-03-22 22:26:00 +0300

    - error-fix

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider, BrowserRouter, Navigate } from "react-router-dom";
 import { Asteroid } from "./pages/Asteroid";
 import { Destroyment } from "./pages/Destroyment";
 import { Asteroids } from "./pages/Asteroids";
 import { AsteroidsContextProvider } from './components/AsteroidCard/AsteroidContext/AsteroidsContext';
 
-const router = createHashRouter([
+const router = createHashRouter(
+  [
     { path: "/", element: <Navigate to="/asteroids" replace /> },
     { path: "/asteroids", element: <Asteroids /> },
     { path: "/destroyment", element: <Destroyment /> },
