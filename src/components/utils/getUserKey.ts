e097f0779bd75dbc6fea9bae25a7eadd463a0a38
di330nance <di330nance@gmail.com>
2025-03-22 21:28:15 +0300

    - error-fix

--- src/components/utils/getUserKey.ts
+++ src/components/utils/getUserKey.ts
@@ -1,5 +1,7 @@
 export const getUserKey = () => {
-
+  if (process.env.NODE_ENV === 'development') {
+    return process.env.REACT_APP_NASA_API_KEY;
+  }
   let userKey = '';
   try {
     userKey = localStorage.getItem('API_KEY');
